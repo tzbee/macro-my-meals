82bfbea5420fe8902d1fb1298e98506e8a3a8f38
Amine Tolba <tolbimine@gmail.com>
2019-05-03 07:53:12 +0300

    CSS minification

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,9 +2,14 @@ const path = require('path');
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const TerserJSPlugin = require('terser-webpack-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
 module.exports = merge(common, {
   mode: 'production',
+  optimization: {
+    minimizer: [new TerserJSPlugin({}), new OptimizeCSSAssetsPlugin({})]
+  },
   output: {
     path: path.resolve(__dirname, 'public'),
     filename: '[name].[chunkhash].js'
@@ -16,3 +21,4 @@ module.exports = merge(common, {
     ...common.plugins
   ]
 });
+
